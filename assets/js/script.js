a0d6eafcb7cb9530fc41518dada99aa31e4f6248
diegofranciscodasilva <diegofranciscodasilva@yahoo.com.br>
2024-01-25 14:35:23 -0300

    atualização do script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ const textarea = document.querySelector('textarea')
 const voiceList = document.querySelector('select')
 const speechBtn = document.querySelector('button')
 
-let synth = speechSynthesis;
-let isSpeaking = true;
+let synth = speechSynthesis
+let isSpeaking = true
 
 voices()
 
